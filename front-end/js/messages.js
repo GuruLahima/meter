e28e6526b9131f7396d122535730e7c05762fc01
Ana <mihajloska.ana@gmail.com>
2018-10-27 16:50:26 +0000

    work in progress

--- front-end/js/messages.js
+++ front-end/js/messages.js
@@ -1,4 +1,10 @@
 var messages = [
     "Thot, plz",
-    
+    "Send this thot to Gulag immediately!",
+    "The thot-o-meter almost exploded due to your thottery, thot!",
+    "A penny for your thots, thot.",
+    "I spot a THOT!",
+    "Begone, thot",
+    "Contain your thottery, thot!",
+    ""
     ];
