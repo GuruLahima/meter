b496fb84fe463c75fbdb07ed8d96ceae4c649226
Ana <mihajloska.ana@gmail.com>
2018-10-27 18:45:00 +0000

    work in progress

--- front-end/js/messages.js
+++ front-end/js/messages.js
@@ -1,10 +1,18 @@
 var messages = [
     "Thot, plz",
     "Send this thot to Gulag immediately!",
-    "The thot-o-meter almost exploded due to your thottery, thot!",
-    "A penny for your thots, thot.",
+    "The thot-o-meter almost exploded due to your thotness!",
+    "A penny for your thots.",
     "I spot a THOT!",
     "Begone, thot",
-    "Contain your thottery, thot!",
-    ""
+    "Contain your thottery!",
+    "You thot you wouldn't reach maximum thotness? I guess you should have thot twice!",
+    "Weren't you the lead role in 'Big impact into Uranus'?",
+    "Check out thot in the desert! Oh, wait, they are sitting in front of the screen!",
+    "'You can touch this' - MC Thot",
+    "Obviously you were the main inspiration for the book of Thotology",
+    "Pink Floyd don't, but you definitely need thot control!",
+    "Who you gonna call? Definitely not Thotbusters!",
+    "If somebody calls out your name in front of the mirror three times, a thot will appear",
+    
     ];
